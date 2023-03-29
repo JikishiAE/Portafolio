d07f018bc14bb04983ba4dbe1fc6ca0e4673c90a
Jikishi <oa624488@gmail.com>
2023-03-29 15:10:09 -0600

    Portafolio V1

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutComponent,
-    SkillsComponent
+    SkillsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- Portfolio/src/app/skills/skills.component.ts
+++ Portfolio/src/app/skills/skills.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent {
+  skills: any[] = [];
 
+
+  ngOnInit(): void {
+    this.skills = [
+      {
+        NOMBRE: ".NET",
+        DESCRIPCION: "2 años de experiencia"
+      },
+      {
+        NOMBRE: "ANGULAR",
+        DESCRIPCION: "2 años de experiencia"
+      },
+      {
+        NOMBRE: "SQL SERVER",
+        DESCRIPCION: "2 años de experiencia"
+      },
+      {
+        NOMBRE: "DESARROLLO DE API",
+        DESCRIPCION: "2 años de experiencia"
+      },
+      {
+        NOMBRE: "MACHINE LEARNING",
+        DESCRIPCION: "Experiencia por proyectos personales"
+      },
+      {
+        NOMBRE: "ERP",
+        DESCRIPCION: "2 años de experiencia"
+      },
+    ]
+  }
 }
